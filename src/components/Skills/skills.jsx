3927979fdbb25d0d0f329add5f21c83a3b0176cb
Blandine M <150096085+BlandineM2508@users.noreply.github.com>
2024-11-14 22:50:02 +0100

    Maj Skills

--- src/components/Skills/skills.jsx
+++ src/components/Skills/skills.jsx
@@ -20,13 +20,13 @@ function Skills() {
           <img alt="icone javascript" className="icon" src={Js} />
           <p className="text">Utilisation des bases du langage CSS.</p>
         </div>
-        <div className="info BoxShadow_Blue">
-          <img alt="icone react" className="icon" src={ReactIcon} />
-          <p className="text">Utilisation de React.</p>
-        </div>
         <div className="info BoxShadow_Pink">
           <img alt="icone sass" className="icon" src={Sass} />
           <p className="text">Utilisation du préprocesseur SASS.</p>
+        </div>
+        <div className="info BoxShadow_Blue">
+          <img alt="icone react" className="icon" src={ReactIcon} />
+          <p className="text">Utilisation de React.</p>
         </div>
         <div className="info BoxShadow_Purple">
           <img alt="icone redux" className="icon" src={Redux} />
@@ -40,10 +40,10 @@ function Skills() {
             Utilisation de Git et GitHub afin de versionner mon travail.
           </p>
         </div>
-        <div className="info BoxShadow_Green">
+        <div className="info BoxShadow_Red">
           <img alt="icone figma" className="icon " src={Figma} />
           <p className="text">
-            Utilisation de Git et GitHub afin de versionner mon travail.
+            Utilisation de Figma pour la création de maquettes.
           </p>
         </div>
       </div>

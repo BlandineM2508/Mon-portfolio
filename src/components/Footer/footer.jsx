20a7e897cd68cdcf6798408f3f33c4f55c12e1d6
Blandine M <150096085+BlandineM2508@users.noreply.github.com>
2024-11-21 16:06:08 +0100

    Update section contact

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -4,11 +4,24 @@ import Arrow from '../../assets/top_arrow.png'
 function Footer() {
   return (
     <div className="footerContainer">
-      <div className="footerText">
-        <p className="brand">Blandine Maurice © 2024</p>
-        <p className="vercel">Hébergé avec Vercel</p>
+      <div className="footerContent">
+        <div className="textColumn">
+          <p className="brand">
+            Blandine Maurice © 2024 <br />
+            Hébergé avec Vercel
+          </p>
+        </div>
+        <div className="linkColumn">
+          <a
+            className="link"
+            href="https://www.linkedin.com/in/blandine-maurice-99a49a151/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Linkedin
+          </a>
+        </div>
       </div>
-
       <a href="#top">
         <img
           src={Arrow}
